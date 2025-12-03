7bdcae27c5f52f9f1928edfb345e486f9ec04fda
Ariel Zvinowanda <kazan@outlook.ph>
2025-12-03 20:54:11 +0200

    Calculator Made

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,46 @@
 #include <bits/stdc++.h>
 
 int main() {
-  std::println("Hello World");
+  bool calcIsRunning = true;
+  double a, b;
+  double result{};
+  char op;
+
+  while (calcIsRunning) {
+    std::println("My Cool Calculator");
+    std::print("Enter a number: ");
+    std::cin >> a;
+
+    std::print("Enter a another number: ");
+    std::cin >> b;
+
+    std::print("Enter a operator or (q to quit): ");
+    std::cin >> op;
+
+    if (op == 'q' || op == 'Q') {
+      calcIsRunning = false;
+      continue;
+    }
+
+    switch (op) {
+      case '+':
+        result = a + b;
+        break;
+      case '-':
+        result = a - b;
+        break;
+      case '*':
+        result = a * b;
+        break;
+      case '/':
+        result = a / b;
+        break;
+      default:
+        std::println("Thats not an operator...");
+    }
+
+    std::print("Result: {}\n", result);
+
+  }
   return 0;
 }
